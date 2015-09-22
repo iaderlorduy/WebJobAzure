d05c9e2bd2f5682c6d404ded36d4aee80275b369
iaderlorduy <iaderlor@gmail.com>
2015-09-22 11:29:25 -0500

    implementación

---
+++ WebJobTest/Functions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+
+namespace WebJobTest
+{
+    public class Functions
+    {
+        // This function will get triggered/executed when a new message is written 
+        // on an Azure Queue called queue.
+        public static void ProcessQueueMessage([QueueTrigger("afro")] String message, TextWriter log, IBinder binder, Int32 dequeueCount)
+        {
+            log.WriteLine("este es el intento numero{0}", dequeueCount);
+            if (dequeueCount <= 5)
+            {
+                binder.Bind<QueueAttribute>(new QueueAttribute("afro-poison"));
+            }
+            throw new Exception("throw perra");
+        }
+
+        public static void ProcessQueueMessagePoison([QueueTrigger("afro-poison")] String message, TextWriter log)
+        {
+            log.WriteLine("afro envenenado");
+        }
+    }
+}
---
+++ WebJobTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+
+namespace WebJobTest
+{
+    // To learn more about Microsoft Azure WebJobs SDK, please see http://go.microsoft.com/fwlink/?LinkID=320976
+    class Program
+    {
+        // Please set the following connection strings in app.config for this WebJob to run:
+        // AzureWebJobsDashboard and AzureWebJobsStorage
+        static void Main()
+        {
+            var conecction = CloudConfigurationManager.GetSetting("AzureWebJobsDashboard");
+            var storageAccount = CloudStorageAccount.Parse(conecction);
+
+            var createClound = storageAccount.CreateCloudQueueClient();
+            var queueReference = createClound.GetQueueReference("afro");
+            var queueReferenceposion = createClound.GetQueueReference("afro-poison");
+
+            queueReference.CreateIfNotExists();
+            queueReferenceposion.CreateIfNotExists();
+
+            var host = new JobHost();
+            // The following code ensures that the WebJob will be running continuously
+            host.RunAndBlock();
+        }
+    }
+}
+
